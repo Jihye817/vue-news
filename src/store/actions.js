a603ecb94650097908a15143bd2dde79d09c7074
ParkJihye <jhpark@beyless.com>
2020-09-29 10:20:18 +0900

    200929 mixin

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,36 +1,36 @@
-import { fetchNewsList, fetchAskList, fetchJobsList, fetchUserInfo, fetchCommentItem } from '../api/index.js';
+import { fetchUserInfo, fetchCommentItem, fetchList } from '../api/index.js';
 
 export default {
-  FETCH_NEWS(context) {
-    fetchNewsList()
-      .then(response => {
-        console.log(response);
-        context.commit('SET_NEWS', response.data);
-        return response;
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  },
-  FETCH_ASK(context) {
-    fetchAskList()
-      .then(response => {
-        console.log(response);
-        context.commit('SET_ASK', response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  },
-  FETCH_JOBS({commit}) {
-    fetchJobsList()
-      .then(({data}) => {
-        commit('SET_JOBS', data);
-      })
-      .catch(error => {
-        console.log(error);
-      })
-  },
+//   FETCH_NEWS(context) {
+//     fetchNewsList()
+//       .then(response => {
+//         console.log(response);
+//         context.commit('SET_NEWS', response.data);
+//         return response;
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       })
+//   },
+//   FETCH_ASK(context) {
+//     fetchAskList()
+//       .then(response => {
+//         console.log(response);
+//         context.commit('SET_ASK', response.data);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       })
+//   },
+//   FETCH_JOBS({commit}) {
+//     fetchJobsList()
+//       .then(({data}) => {
+//         commit('SET_JOBS', data);
+//       })
+//       .catch(error => {
+//         console.log(error);
+//       })
+//   },
   FETCH_USER({commit}, name) {
     fetchUserInfo(name)
       .then(({data}) => {
@@ -48,5 +48,10 @@ export default {
       .catch(error => {
         console.log(error);
       })
-  }
+  },
+  FETCH_LIST({commit}, pageName) {
+    fetchList(pageName)
+      .then(({data}) => commit('SET_LIST', data))
+      .catch(error => console.log(error));
+  },
 }
